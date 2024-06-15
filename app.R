d4c2fe7738e962de57995331953b7e03e190d023
shannonmansfield <mansfis4@wwu.edu>
2024-06-14 23:13:32 -0700

    Update app.R final

--- app.R
+++ app.R
@@ -42,7 +42,7 @@ ui <- page_navbar( # set up navbar to link to different pages
               '. I got the idea to work with this data set from the ', 
               a(href = 'https://github.com/rfordatascience/tidytuesday/tree/master/data/2020/2020-11-24', 
                 'TidyTuesday GitHub page', .noWS = "outside"), 
-              ". I used their scraping and cleaning script, but I did edit it to grab a few more things, like the 
+              ". I used their scraping and cleaning script, but I did edit it to scrape a few more things, like the 
               link URL for each hike.",
               .noWS = c("after-begin", "before-end")),
               
@@ -53,7 +53,7 @@ ui <- page_navbar( # set up navbar to link to different pages
                  and have one recommended</li>
                  <li>A table of all of the hikes in the data set, where you can sort by various attributes and use a search bar 
                  to search for something more specific</li>
-                 <li>A page with interactive plots, where you can choose variables and see how they vary--e.g. see how user ratings
+                 <li>A page with interactive plots, where you can see how different variables are distributed--e.g. see how user ratings
                  vary across different general locations</li>
                  </ul>"),
             
@@ -66,7 +66,7 @@ ui <- page_navbar( # set up navbar to link to different pages
             theme = setTheme,
             
             p("Here you can have a trail recommended to you based on your specifications. Use the sliders and input boxes to 
-              make your choices, hit 'Find a hike', and a suggestion will appear. Click the button again for more suggestions."),
+              make your choices, and a suggestion will appear. Click the 'Find a hike!' button for more suggestions."),
             
             p("Note that I have truncated some of the sliders--e.g. the the longest trail in the data set is 295 miles, 
             but I have cut off the slider at 50 miles here to make it easier to use for most hikes. If you are looking 
@@ -74,7 +74,7 @@ ui <- page_navbar( # set up navbar to link to different pages
             or gain columns from high to low."),
             
             p("All locations are selected by default. To remove locations, click one and hit delete or backspace. They can 
-              then be selected again from the drop-down menu."),
+              then be selected again from the drop-down menu. Multiple features can also be selected at once."),
             
             hr(),
             
@@ -110,14 +110,13 @@ ui <- page_navbar( # set up navbar to link to different pages
                 selectInput("locationC",
                         "General location",
                         multiple = TRUE,
-                        selected = c("North Cascades", "Central Cascades", "Snoqualmie Region", "Eastern Washington",
-                                     "Olympic Peninsula", "Issaquah Alps", "South Cascades", "Central Washington", 
-                                     "Mount Rainier Area", "Southwest Washington", "Puget Sound and Islands"),
+                        selected = locations_list,
                         choices = locations_list),
                 
                 selectInput("featuresC",
                             "Features",
                             multiple = TRUE,
+                            selected = NA,
                             choices = features_list),
                 
                 width = 3
@@ -129,27 +128,17 @@ ui <- page_navbar( # set up navbar to link to different pages
           
           actionButton("start", "Find a hike!"),
 
-          p("Your recommended hike is ",
-            textOutput(outputId = "name", inline = TRUE), ", with a length of ",
-            textOutput(outputId = "length", inline = TRUE), ", an elevation gain of ",
-            textOutput(outputId = "gain", inline = TRUE), "feet, and a high point of ",
-            textOutput(outputId = "highpoint", inline = TRUE), "feet."),
-
-          p("The general location is ",
-            textOutput(outputId = "location", inline = TRUE), " and the average user rating is ",
-            textOutput(outputId = "rating", inline = TRUE), " out of 5 stars."),
-
-          p("A short description follows:"),
-
-          textOutput("description"),
-
+          textOutput("line1"),
           br(),
-          p("Find out more by visiting the link below!"),
-
-          textOutput("url")
-            
-          
-          
+          textOutput("line2"),
+          br(),
+          textOutput("line3"),
+          br(),
+          textOutput("line4"),
+          br(),
+          textOutput("line5"),
+          br(),
+          textOutput("line6")
         )
     )),
 
@@ -165,26 +154,39 @@ ui <- page_navbar( # set up navbar to link to different pages
   
   nav_panel(title = "Plots",
             theme = setTheme,
-    plotOutput("plot"),
-    p("I'm planning to do more with the analysis of the data, looking at the associations between different variables. 
-    I spent most of my time getting the recommendations to work... 
-      Here is just an example looking at the distribution of the hike ratings by general location. I excluded hikes with 
-      a zero rating because that typically meant that the hike did not have any ratings yet, which was skewing the data.")
-))
+            
+            p("Here you can see a few plots, breaking down the distributions of a couple attributes. For the plots involving user 
+            ratings, I excluded hikes with a zero rating because that typically meant that the hike did not have any ratings yet, 
+              which was skewing the data. For the lengths plot I similarly cut it off at 25 miles because they were just so skewed 
+              by a few very long trails."),
+            
+            p("The first plot allows your to see how the average user ratings are distributed for different general locations. 
+              I wanted to see if any were particularly higher or lower than the others. Most of the distributions look fairly similar
+              and seem to peak around 4, although it looks like the Issaquah Alps and South Cascades regions peak a little lower."),
+            
+            p("I was also curious whether there would be a difference between ratings from one-way and roundtrip trails, which can be 
+              seen in the second plot. It does seem like roundtrip trails have a higher peak rating."),
+            
+            p("The third plot shows how the lengths of the hikes vary across locations. Most have a quite similar distribution, but 
+              the Puget Sound and Islands are and the Issaquah Alps area tend to have much shorter hikes."),
+            
+            sidebarLayout(
+              sidebarPanel(
+                selectInput("y", "Choose what to plot", c("Ratings by location" = "ratings1", 
+                                                          "Ratings by trip type" = "ratings2",
+                                                          "Length distribution by location" = "lengths")),
+                width = 3
+              ),
+              mainPanel(
+                plotOutput("myPlot"), 
+                width = 9
+              ),
+              
+              
+              
+            )
 
-hike_plot <- ggplot(clean_hike_data) + 
-  facet_wrap(~location_general, scales = "free_x") +
-  geom_density(mapping = aes(rating), color = "#4A7628", linewidth = 1) +
-  scale_x_continuous(expand = c(0, 0), limits = c(0.1, NA)) +
-  scale_y_continuous(expand = c(0, 0)) +
-  theme_bw() +
-  labs(x = "Rating (out of five)", y = "") +
-  theme(strip.background = element_blank()) +
-  theme(text = element_text(size = 20),
-        plot.title = element_text(size = 24),
-        panel.grid.major.x = element_blank(),
-        panel.grid.minor.x = element_blank()) +
-  ggtitle("Ratings distribution by location")
+))
 
 # define recommender function
 recommender <- function(clean_hike_data, ratingC, lengthC, gainC, highpointC, locationC, featuresC) {
@@ -196,6 +198,19 @@ recommender <- function(clean_hike_data, ratingC, lengthC, gainC, highpointC, lo
     filter(gain >= gainC[1] & gain <= gainC[2]) %>%
     filter(highpoint >= highpointC[1] & highpoint <= highpointC[2]) %>%
     filter(location_general %in% locationC) 
+  
+  saved$myFlag <- rep(FALSE, nrow(saved))
+  
+  for (i in 1:nrow(saved)) {
+    if(all(featuresC %in% saved$features[[i]])) {
+      saved$myFlag[i] <- TRUE
+    }
+    else {
+      saved$myFlag[i] <- FALSE
+    }
+  }
+  
+  saved <- saved %>% filter(myFlag == TRUE)
   
   return(saved)
 }
@@ -216,15 +231,31 @@ server <- function(input, output) {
     selected()[sample(1:length(selected()), 1), ]
   )
   
-  output$name <- renderText(choice()$name)
-  output$length <- renderText(choice()$length)
-  output$trip <- renderText(choice()$trip)
-  output$gain <- renderText(choice()$gain)
-  output$highpoint <- renderText(choice()$highpoint)
-  output$location <- renderText(choice()$location)
-  output$url <- renderText(choice()$url)
-  output$rating <- renderText(choice()$rating)
-  output$description <- renderText(choice()$description)
+  output$line1 <- renderText({paste("Your recommended hike is ", 
+                                    choice()$name, 
+                                    ", with a length of ", 
+                                    choice()$length, 
+                                    ", an elevation gain of ", 
+                                    choice()$gain, 
+                                    "feet, and a high point of ",
+                                    choice()$highpoint, 
+                                    "feet.")
+      }
+  )
+  
+  output$line2 <- renderText({paste("The general location is ",
+                                  choice()$location, 
+                                  " and the average user rating is ",
+                                  choice()$rating, 
+                                  " out of 5 stars.")})
+  
+  output$line3 <- renderText("A short description follows:")
+  
+  output$line4 <- renderText(choice()$description)
+
+  output$line5 <- renderText("Find out more by visiting the link below!")
+
+  output$line6 <- renderText(choice()$url)
 
   output$table <- renderReactable(
     # create the reactable
@@ -232,7 +263,7 @@ server <- function(input, output) {
               columns = list(
                 name = colDef(name = "Hike name", width = 150),
                 location = colDef(name = "Location", width = 200),
-                length_total = colDef(name = "Total length (mi)", width = 150),
+                length_total = colDef(name = "Total length (mi)", width = 125),
                 trip = colDef(name = "Trip type", width = 100),
                 gain = colDef(name = "Elevation gain (ft)", width = 175),
                 highpoint = colDef(name = "High point (ft)", width = 150),
@@ -245,8 +276,53 @@ server <- function(input, output) {
               searchable = TRUE,
               sortable = TRUE))
   
-  output$plot <- renderPlot(hike_plot,
-                            height = 700)
+  output$myPlot <- renderPlot({
+    if (input$y == "ratings1") {
+      ggplot(clean_hike_data) + 
+        facet_wrap(~location_general) +
+        geom_density(mapping = aes(rating), color = "#4A7628", linewidth = 1) +
+        scale_x_continuous(expand = c(0, 0), limits = c(0.1, NA)) +
+        scale_y_continuous(expand = c(0, 0)) +
+        theme_bw() +
+        labs(x = "Rating (out of five)", y = "") +
+        theme(strip.background = element_blank()) +
+        theme(text = element_text(size = 20),
+              plot.title = element_text(size = 24),
+              panel.grid.major.x = element_blank(),
+              panel.grid.minor.x = element_blank()) +
+        ggtitle("Ratings distribution by location")
+    } else if (input$y == "ratings2") {
+      ggplot(clean_hike_data) + 
+        facet_wrap(~trip) +
+        geom_density(mapping = aes(rating), color = "#E9A82B", linewidth = 1) +
+        scale_x_continuous(expand = c(0, 0), limits = c(0.1, NA)) +
+        scale_y_continuous(expand = c(0, 0)) +
+        theme_bw() +
+        labs(x = "Rating (out of five)", y = "") +
+        theme(strip.background = element_blank()) +
+        theme(text = element_text(size = 20),
+              plot.title = element_text(size = 24),
+              panel.grid.major.x = element_blank(),
+              panel.grid.minor.x = element_blank()) +
+        ggtitle("Ratings distribution by location")
+    }
+    else {
+      ggplot(clean_hike_data) + 
+        facet_wrap(~location_general) +
+        geom_density(mapping = aes(length_total), color = "#003029", linewidth = 1) +
+        scale_x_continuous(expand = c(0, 0), limits = c(0, 25)) +
+        scale_y_continuous(expand = c(0, 0)) +
+        theme_bw() +
+        labs(x = "Length (mi)", y = "") +
+        theme(strip.background = element_blank()) +
+        theme(text = element_text(size = 20),
+              plot.title = element_text(size = 24),
+              panel.grid.major.x = element_blank(),
+              panel.grid.minor.x = element_blank()) +
+        ggtitle("Total length distribution by location")
+    }},
+                              height = 700,
+                              res = 96)
   
   
 }
